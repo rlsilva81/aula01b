1aca364cdd2955ce2e956fba47c26398dd441e22
rlsilva81 <romulo.silva@restinga.ifrs.edu.br>
2021-04-27 09:59:12 -0300

    projeto_kit

---
+++ src/aula01b/first_help.java
@@ -0,0 +1,10 @@
+package aula01b;
+
+public class first_help {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author romulo
+ *
+ */
+module aula01b {
+}
